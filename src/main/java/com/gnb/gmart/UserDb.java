f11dff63a4797c83152aefbda8db1d479e30810e
gananath <ganesh.jpa4@gmail.com>
2020-07-20 16:42:48 +0530

    added a method to userdb

--- src/main/java/com/gnb/gmart/UserDb.java
+++ src/main/java/com/gnb/gmart/UserDb.java
@@ -1,5 +1,8 @@
 package com.gnb.gmart;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,5 +14,11 @@ public class UserDb {
 			user = new User(1,"Banishree"); 
 		}
 		return user;
+	}
+	public List<User> getAllUsers() {
+		List<User> users = new ArrayList<User>();
+		users.add(new User(2, "Banishree"));
+		users.add(new User(3, "Ami"));
+		return users;
 	}
 }
